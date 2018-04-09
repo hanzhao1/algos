f727738263804b1662de4158abd3a96fb34a1a31
Han <hanshengzhao1993@gmail.com>
2018-04-08 20:57:07 -0700

    added remove iterate methods

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -37,7 +37,32 @@ class LinkedList {
     }
   }
 
-  remove()
+  remove(value){
+    let currentNode = this.head;
+    // removing the first node
+    if(currentNode.data === value){
+      this.head = currentNode.next;
+      return;
+    }
+    let previousNode = currentNode;
+    currentNode = currentNode.next;
+    while(currentNode){
+      if(currentNode.data === value){
+        previousNode.next = currentNode.next;
+      }
+      currentNode = currentNode.next;
+      previousNode = previousNode.next;
+    }
+    return null;
+  }
+
+  iterate(cb){
+    let currentNode = this.head;
+    while(currentNode){
+      cb(currentNode.data)
+      currentNode = currentNode.next;
+    }
+  }
 
 }
 
@@ -48,7 +73,8 @@ list.add(2)
 list.add(4)
 list.add(9)
 
-console.log(list)
 
-console.log(list.searchNodeAt(3))
+// console.log(list.searchNodeAt(3))
+console.log(list.remove(9))
 
+console.log(list.iterate((a)=>{console.log(a)}))
